68ca5170fe8879f24e93fc9d0f78a6813559f251
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-13 19:43:20 -0300

    feat: pointer with *

--- main.go
+++ main.go
@@ -14,6 +14,14 @@ func main() {
 	contaDoGabriel := ContaCorrente{titular: "Gabriel", numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
 
 	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+
 	fmt.Println(contaDoGabriel)
 	fmt.Println(contaDaBruna)
+
+	var contaDaCris *ContaCorrente
+	contaDaCris = new(ContaCorrente)
+	contaDaCris.titular = "Cris"
+	contaDaCris.saldo = 500
+
+	fmt.Println(*contaDaCris)
 }
