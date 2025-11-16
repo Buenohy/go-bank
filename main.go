d904270b7a707b278bc52c671eb6be8bd4199d0a
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-16 16:52:25 -0300

    feat: go modules and a new accounts folder for importing accounts globally

--- main.go
+++ main.go
@@ -1,46 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-bank/contas"
+)
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
 
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Deposito realizado com sucesso", c.saldo
-	} else {
-		return "O valor do deposito é menor que zero", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	} else {
-		return false
-	}
-}
 
 func main() {
-	contaDaSilvia := ContaCorrente{titular: "Silvia", saldo: 300}
-	contaDoGabriel := ContaCorrente{titular: "Gabriel", saldo: 100}
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGabriel := contas.ContaCorrente{Titular: "Gabriel", Saldo: 100}
 
 	staus := contaDaSilvia.Transferir(-200, &contaDoGabriel)
 
