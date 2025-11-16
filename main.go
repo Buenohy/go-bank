2d7e20f8100536009e1b400b518003e23156138b
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-16 17:11:08 -0300

    feat: function Obter Saldo to see value inside the bank account

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"go-bank/clientes"
 	"go-bank/contas"
 )
 
 func main() {
-	clienteBruno := clientes.Titular{"Bruno", "123.123.123-12", "Desenvolvedor Go"}
-	contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 123456, 100}
-	fmt.Println(contaDoBruno)
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
+
+	fmt.Println(contaExemplo.ObterSaldo())
 }
 
